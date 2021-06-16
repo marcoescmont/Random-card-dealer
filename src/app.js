2e853d311f4ac97883841864534b45fd689093d2
marcoescmont <marcoescmont@gmail.com>
2021-06-16 04:18:23 +0000

    add width and height inputs

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function generateCard() {
   //write your code here
   function randomNumber() {
     const randomNumber = Math.floor(Math.random() * 13 + 1);
@@ -16,90 +16,43 @@ window.onload = function() {
     else if (randomNumber === 0) return "A";
     else return randomNumber;
   }
-
-  let randomNumberSet = Math.floor(Math.random() * 4 + 1);
-  if (randomNumberSet === 1) {
-    return (document
-      .querySelector(".card")
-      .appendChild(
-        document.createElement("div")
-      ).innerHTML = `<t class="heart">♥</t><n class="numberR">${randomNumber()}</n><t class="heart2">♥</t>`);
-  } else if (randomNumberSet === 2) {
-    return (document
-      .querySelector(".card")
-      .appendChild(
-        document.createElement("div")
-      ).innerHTML = `<t class="spade">♠</t><n class="numberB">${randomNumber()}</n><t class="spade2">♠</t>`);
-  } else if (randomNumberSet === 3) {
-    return (document
-      .querySelector(".card")
-      .appendChild(
-        document.createElement("div")
-      ).innerHTML = `<t class="diamond">♦</t><n class="numberR">${randomNumber()}</n><t class="diamond2">♦</t>`);
-  } else if (randomNumberSet === 4)
-    return (document
-      .querySelector(".card")
-      .appendChild(
-        document.createElement("div")
-      ).innerHTML = `<t class="club">♣</t><n class="numberB">${randomNumber()}</n><t class="club2">♣</t>`);
-  else
-    return (document
-      .querySelector(".card")
-      .appendChild(
-        document.createElement("div")
-      ).innerHTML = `<t class="heart">♥</t><n class="numberR">${randomNumber()}</n><t class="heart2">♥</t>`);
-};
-
-window.onclick = function() {
-  //write your code here
-  document
-    .querySelector(".card")
-    .appendChild(
-      document.createElement("div")
-    ).innerHTML = `<div class="card"></div>`;
-  function randomNumber() {
-    const randomNumber = Math.floor(Math.random() * 13 + 1);
-    if (randomNumber === 11) return "J";
-    else if (randomNumber === 12) return "Q";
-    else if (randomNumber === 13) return "K";
-    else if (randomNumber === 1) return "A";
-    else if (randomNumber === 0) return "A";
-    else if (randomNumber === 10) {
-      randomNumber;
-      let card10 = document.querySelector(".numberB");
-      card10.style.padding = "0% 60% 0% 0%";
-    } else return randomNumber;
+  function fillerSelector() {
+    let randomNumberSet = Math.floor(Math.random() * 4 + 1);
+    if (randomNumberSet === 1) {
+      return (document
+        .querySelector(".card")
+        .appendChild(
+          document.createElement("div")
+        ).innerHTML = `<t class="heart">♥</t><n class="numberR">${randomNumber()}</n><t class="heart2">♥</t>`);
+    } else if (randomNumberSet === 2) {
+      return (document
+        .querySelector(".card")
+        .appendChild(
+          document.createElement("div")
+        ).innerHTML = `<t class="spade">♠</t><n class="numberB">${randomNumber()}</n><t class="spade2">♠</t>`);
+    } else if (randomNumberSet === 3) {
+      return (document
+        .querySelector(".card")
+        .appendChild(
+          document.createElement("div")
+        ).innerHTML = `<t class="diamond">♦</t><n class="numberR">${randomNumber()}</n><t class="diamond2">♦</t>`);
+    } else if (randomNumberSet === 4)
+      return (document
+        .querySelector(".card")
+        .appendChild(
+          document.createElement("div")
+        ).innerHTML = `<t class="club">♣</t><n class="numberB">${randomNumber()}</n><t class="club2">♣</t>`);
+    else
+      return (document
+        .querySelector(".card")
+        .appendChild(
+          document.createElement("div")
+        ).innerHTML = `<t class="heart">♥</t><n class="numberR">${randomNumber()}</n><t class="heart2">♥</t>`);
   }
 
-  let randomNumberSet = Math.floor(Math.random() * 4 + 1);
-  if (randomNumberSet === 1) {
-    return (document
-      .querySelector(".card")
-      .appendChild(
-        document.createElement("div")
-      ).innerHTML = `<t class="heart">♥</t><n class="numberR">${randomNumber()}</n><t class="heart2">♥</t>`);
-  } else if (randomNumberSet === 2) {
-    return (document
-      .querySelector(".card")
-      .appendChild(
-        document.createElement("div")
-      ).innerHTML = `<t class="spade">♠</t><n class="numberB">${randomNumber()}</n><t class="spade2">♠</t>`);
-  } else if (randomNumberSet === 3) {
-    return (document
-      .querySelector(".card")
-      .appendChild(
-        document.createElement("div")
-      ).innerHTML = `<t class="diamond">♦</t><n class="numberR">${randomNumber()}</n><t class="diamond2">♦</t>`);
-  } else if (randomNumberSet === 4)
-    return (document
-      .querySelector(".card")
-      .appendChild(
-        document.createElement("div")
-      ).innerHTML = `<t class="club">♣</t><n class="numberB">${randomNumber()}</n><t class="club2">♣</t>`);
-  else
-    return (document
-      .querySelector(".card")
-      .appendChild(
-        document.createElement("div")
-      ).innerHTML = `<t class="heart">♥</t><n class="numberR">${randomNumber()}</n><t class="heart2">♥</t>`);
+  return fillerSelector();
 };
+
+setTimeout(function() {
+  window.location.reload(1);
+}, 10000);
