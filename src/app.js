e9f7f3942621448e3ef89aa1bb8d9d7eaa01c033
marcoescmont <marcoescmont@gmail.com>
2021-06-17 00:11:17 +0000

    setting interval to change card after 10s

--- src/app.js
+++ src/app.js
@@ -100,6 +100,54 @@ window.onclick = function() {
   return fillerSelector();
 };
 
-setTimeout(function() {
-  window.location.reload(1);
+setInterval(function() {
+  let element = document.querySelector(".fullCard");
+  return element.parentNode.removeChild(element);
+}, 9999);
+
+setInterval(function() {
+  function randomNumber() {
+    const randomNumber = Math.floor(Math.random() * 13 + 1);
+    if (randomNumber === 11) return "J";
+    else if (randomNumber === 12) return "Q";
+    else if (randomNumber === 13) return "K";
+    else if (randomNumber === 1) return "A";
+    else if (randomNumber === 0) return "A";
+    else return randomNumber;
+  }
+  function fillerSelector() {
+    let randomNumberSet = Math.floor(Math.random() * 4 + 1);
+    if (randomNumberSet === 1) {
+      return (document
+        .querySelector(".card")
+        .appendChild(
+          document.createElement("div")
+        ).innerHTML = `<div class="fullCard"><t class="heart">♥</t><n class="numberR">${randomNumber()}</n><t class="heart2">♥</t></div>`);
+    } else if (randomNumberSet === 2) {
+      return (document
+        .querySelector(".card")
+        .appendChild(
+          document.createElement("div")
+        ).innerHTML = `<div class="fullCard"><t class="spade">♠</t><n class="numberB">${randomNumber()}</n><t class="spade2">♠</t></div>`);
+    } else if (randomNumberSet === 3) {
+      return (document
+        .querySelector(".card")
+        .appendChild(
+          document.createElement("div")
+        ).innerHTML = `<div class="fullCard"><t class="diamond">♦</t><n class="numberR">${randomNumber()}</n><t class="diamond2">♦</t></div>`);
+    } else if (randomNumberSet === 4)
+      return (document
+        .querySelector(".card")
+        .appendChild(
+          document.createElement("div")
+        ).innerHTML = `<div class="fullCard"><t class="club">♣</t><n class="numberB">${randomNumber()}</n><t class="club2">♣</t></div>`);
+    else
+      return (document
+        .querySelector(".card")
+        .appendChild(
+          document.createElement("div")
+        ).innerHTML = `<div class="fullCard"><t class="heart">♥</t><n class="numberR">${randomNumber()}</n><t class="heart2">♥</t></div>`);
+  }
+
+  return fillerSelector();
 }, 10000);
